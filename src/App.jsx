1128174898b3661fbbc0a6d7bdb77bd04b64c838
Daniel Thurber <dthurber@xcures.com>
2024-04-22 18:23:20 -0400

    Basic Layout
    Baseline commit with basic game layout and components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Game from './components/Scrambl'
+
+function App() {
+
+  return (
+    <Game />
+  )
+}
+
+export default App
---
+++ src/components/Scrambl.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div className="home-container">
+      <div className="scrambl-header-logo">
+        SCRAMBL
+      </div>
+
+      <div className="reveal">
+        <button className="reveal-button">Lets Go!</button>
+      </div>
+    </div>
+  )
+}
+
+export default Game
