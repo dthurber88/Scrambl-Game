2c52bd3b3c6a1a6ac90db69494e9445db0f5b87e
dthurber88 <dthurber88@gmail.com>
2024-04-26 19:57:59 -0400

    Update Footer.jsx
    Added linkedin profile link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      by Daniel Thurber{/* by <img src="src\assets\ferret-logo-tpx.png" alt="Ferrita Games" /> */}
+      by&nbsp;
+      <a href="https://www.linkedin.com/in/danielthurber/">Daniel Thurber</a>
+      {/* by <img src="src\assets\ferret-logo-tpx.png" alt="Ferrita Games" /> */}
     </div>
   );
 };
